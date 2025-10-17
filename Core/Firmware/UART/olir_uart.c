2229ccaeabaaf301a31f9f78d7fe6d39268e2f8f
olireswar <olireasu@gmail.com>
2025-10-17 16:52:20 -0400

    uses wrapper functions and pipes

--- Core/Firmware/UART/olir_uart.c
+++ Core/Firmware/UART/olir_uart.c
@@ -10,6 +10,7 @@
 #define UART_DEVICE_NAME DT_LABEL(DT_NODELABEL(uart0))
 #define RING_BUF_SIZE 1024
 #define RX_BUF_SIZE 64
+#define PIPE_BUF_SIZE 256
 
 RING_BUF_DECLARE(uart_ringbuf, RING_BUF_SIZE);
 static uint8_t rx_buf[RX_BUF_SIZE];
@@ -22,10 +23,15 @@ static const struct device *uart_dev;
 K_THREAD_STACK_DEFINE(dma_thread_stack, THREAD_STACK_SIZE);
 K_THREAD_STACK_DEFINE(poll_thread_stack, THREAD_STACK_SIZE);
 
-static struct k_thread dma_thread_data;
-static struct k_thread poll_thread_data;
+static struct z_thread_t dma_thread_data;
+static struct z_thread_t poll_thread_data;
 
 static struct z_sem_t z_tx_done_sem;
+
+// Allocate a buffer for the pipe, aligned to 4 bytes
+unsigned char __aligned(4) pipe_buffer[PIPE_SIZE];
+
+static struct z_pipe_t uart_pipe;
 
 const struct device *console_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_console));
 int data_ready = false;
@@ -38,6 +44,9 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
     switch (evt->type) {
     case UART_RX_RDY: {
         data_ready = true;
+
+        /* we might need another wrapper function to make sure the pipe's buffer doesn't get filled
+
         size_t len = evt->data.rx.len;
         size_t space = ring_buf_space_get(&uart_ringbuf);
 
@@ -45,8 +54,13 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
         if (len > space) {
             ring_buf_get(&uart_ringbuf, NULL, len - space);
         }
+        */
 
-        ring_buf_put(&uart_ringbuf, &rx_buf[evt->data.rx.offset], len);
+        size_t bytes_written;
+        // Non-blocking put; handle overflow as needed
+        pipe_put(&uart_pipe, &rx_buf[evt->data.rx.offset], len,
+                   &bytes_written, K_NO_WAIT);
+
         break;
     }
     case UART_RX_DISABLED:
@@ -75,6 +89,8 @@ static void uart_async_init(void)
     }
 
     sem_init(&z_tx_done_sem, 0, 1);
+    pipe_init(&uart_pipe, pipe_buffer, PIPE_BUF_SIZE);
+
     uart_callback_set(uart_dev, uart_callback, NULL);
     uart_rx_enable(uart_dev, rx_buf, RX_BUF_SIZE, 50);
 }
@@ -95,11 +111,12 @@ void dma_thread(void *a, void *b, void *c)
 void poll_thread(void *a, void *b, void *c)
 {
     uint8_t buffer[128];
+    size_t bytes_written;
     size_t len;
 
     while (1) {
         k_sleep(K_MSEC(500));
-        len = poll_uart_ring_buffer(buffer, sizeof(buffer));
+        len = pipe_get(&uart_pipe, buffer, sizeof(buffer), &bytes_written, K_NO_WAIT);
         if (len > 0) {
             printk("Read %u bytes: ", len);
             for (size_t i = 0; i < len; i++) {
@@ -130,11 +147,11 @@ void main(void)
 {
     printk("Starting async UART DMA example\n");
 
-    k_thread_create(&dma_thread_data, dma_thread_stack, THREAD_STACK_SIZE,
+    thread_start(&dma_thread_data, dma_thread_stack, THREAD_STACK_SIZE,
                     dma_thread, NULL, NULL, NULL,
                     DMA_THREAD_PRIORITY, 0, K_NO_WAIT);
 
-    k_thread_create(&poll_thread_data, poll_thread_stack, THREAD_STACK_SIZE,
+    thread_start(&poll_thread_data, poll_thread_stack, THREAD_STACK_SIZE,
                     poll_thread, NULL, NULL, NULL,
                     POLL_THREAD_PRIORITY, 0, K_NO_WAIT);
 }
